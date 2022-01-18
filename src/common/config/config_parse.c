fc981947dc4a069d6d8657c4b282d501f044a3f9
bgenia <bgenia@student.21-school.ru>
2022-01-18 15:33:11 +0300

    Implement argument validation

--- src/common/config/config_parse.c
+++ src/common/config/config_parse.c
@@ -6,17 +6,37 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 15:13:10 by bgenia            #+#    #+#             */
-/*   Updated: 2022/01/13 18:04:40 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/01/18 15:32:15 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philosophers/lib/lib.h>
 #include <philosophers/mandatory/simulation.h>
 
+static int
+	_validate_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+			if (!ft_isdigit(argv[i][j++]))
+				return (-1);
+		i++;
+	}
+	return (0);
+}
+
 int
 	config_parse(t_config *config, int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
+		return (-1);
+	if (_validate_args(argc, argv))
 		return (-1);
 	config->philo_count = ft_atoi(argv[1]);
 	config->time_to_die = ft_atoi(argv[2]);
