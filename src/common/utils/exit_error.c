dfb88d845612e7d8dcd43cc29eccf05e52c85939
bgenia <bgenia@student.21-school.ru>
2022-01-18 15:34:10 +0300

    Remove redundant linebreaks in exit_error

--- src/common/utils/exit_error.c
+++ src/common/utils/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 14:05:50 by bgenia            #+#    #+#             */
-/*   Updated: 2022/01/14 17:04:33 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/01/18 15:31:55 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 int
 	exit_error(int code, char *message)
 {
-	ft_putendl_fd(TERM_F_RED, STDERR_FILENO);
+	ft_putstr_fd(TERM_F_RED, STDERR_FILENO);
 	ft_putendl_fd(message, STDERR_FILENO);
-	ft_putendl_fd(TERM_RESET, STDERR_FILENO);
+	ft_putstr_fd(TERM_RESET, STDERR_FILENO);
 	return (code);
 }
